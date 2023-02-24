bf861008d196480b86d46d5b7a8361908a11c36a
Aacash Srinath Nambiar <100955640+Aacash-Srinath@users.noreply.github.com>
2023-02-24 21:03:35 +0530

    Update script.js

--- Design-Elements/Button-Element/script.js
+++ Design-Elements/Button-Element/script.js
@@ -1,1 +1,7 @@
-javascript
+const btn = document.querySelector("#btn");
+const text = document.querySelector("#text");
+
+btn.onclick = () => {
+    text.innerHTML = ("Thank You");
+    btn.classList.add("active");
+};
